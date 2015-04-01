2b005b4233adb4f2db4febec86f00f0575a445ef
ns <ns@ns-no-MacBook-Air.local>
2015-04-02 00:39:52 +0900

    2nd

--- main.js
+++ main.js
@@ -1,58 +1,85 @@
 var app = angular.module('mainModule', []);
 
-app.controller('amazon_box', function($scope, autocomplete,amazonSearch) {
-  $scope.words = autocomplete;
- // $scope.results = amazonSearch;
+app
+  .value('constants', {
+    //your own aws keys must be used here.
+    AWSAccessKeyId: "AKIAIPTRW2ANAGYGXUVA",
+    AWSsecretkeyId: "te6dvgK1yFAZ2tfM8ahLKEHjW8acpqbWU75Nn2qA",
+    URI: "ecs.amazonaws.jp"
+  })
+  .value('options', {
+    //These parameters can be changed for your purposes
+    AssociateTag: "feynman123-22",
+    Operation: "ItemSearch",
+    SearchIndex: "Books",
+  });
 
-});
 
-app.factory('autocomplete', function($scope, $http) {
-  $scope.autocomplete = function(keyword) {
+app
+  .factory('autocomplete', function($http) {
 
-    $http.jsonp(
-      'http://completion.amazon.co.jp/search/complete', {
-        params: {
-          callback: 'JSON_CALLBACK',
-          mkt: '6',
-          method: 'completion',
-          'search-alias': 'stripbooks',
-          q: keyword,
-        },
-        timeout: 5000
+    return function(keyword, callback) {
+      $http.jsonp(
+        "http://completion.amazon.co.jp/search/complete", {
+          params: {
+            callback: 'JSON_CALLBACK',
+            mkt: '6',
+            method: 'completion',
+            'search-alias': 'stripbooks',
+            q: keyword,
+          },
+          timeout: 5000
+        }).
+      success(function(data, status) {
+        callback(status, data[1]);
       }).
-    success(function(data, status) {
-      $scope.status = status;
-      $scope.data = data;
-      return data[1];
-      // $scope.words = data[1];
-    }).
-    error(function(data, status) {
-      $scope.data = data || 'gcRequest failed';
-      $scope.status = status;
-    });
-  };
+      error(function(data, status) {
+        data = data || "Request failed";
+        callback(status, data);
+      });
+    };
+  })
 
-});
-
-/*
-app.factory('amazonSearch', function($scope, $http) {
-
-  $scope.search = function(keyword) {
-    $http.get(
-      request, {
+.factory('search', function(constants, options, $http) {
+  return {
+    itemsearch: function(keyword, callback) {
+      var request = getSignedQuery(constants, options, keyword);
+      var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from xml where url="' + request + '"') + '&format=json';
+//Get JSON data via yahoo! YQL service
+      $http({
+        method: 'JSONP',
+        url: yql,
         params: {
-        },
-        timeout: 5000
+          callback: 'JSON_CALLBACK'
+        }
       }).
-    success(function(data, status) {
-      $scope.status2 = status;
-      $scope.results = data;
-    }).
-    error(function(data, status) {
-      $scope.results = data || 'gcRequest failed';
-      $scope.status2 = status;
-    });
+      success(function(data, status) {
+        callback(status, data);
+      }).
+      error(function(data, status, headers, config) {
+        callback(status, data);
+        //$scope.data = data || "request failed";
+        //$scope.status = status;
+      });
+    }
   };
-
 });
-*/
+
+
+app
+  .controller('mainCtrl', function($scope, autocomplete, search) {
+    $scope.onChange = function(keyword) {
+      autocomplete(keyword, function(status, words) {
+        $scope.words = words;
+        $scope.status = status;
+      });
+    };
+
+    $scope.search = function(keyword) {
+      search.itemsearch(keyword, function(status, data) {
+        $scope.result = data;
+        $scope.items = data.query.results.ItemSearchResponse.Items.Item;
+        $scope.status = status;
+      });
+    };
+  });
